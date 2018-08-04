36e34570008c1729ebbf0ef76e90afc5fb842beb
Federico Cingolani <mail@fcingolani.com.ar>
2018-08-03 22:38:30 -0300

    Se agrega calendario de Uruguay

--- src/lib/calendar.js
+++ src/lib/calendar.js
@@ -1,9 +1,11 @@
 import argentina from 'data/argentina.yml'
 import chile from 'data/chile.yml'
+import uruguay from 'data/uruguay.yml'
 
 export default {
 
   ar: argentina,
-  cl: chile
+  cl: chile,
+  uy: uruguay,
 
 }
